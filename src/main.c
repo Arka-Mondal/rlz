c3efdad5072d7c09b612835439058bc796902c8b
Arka Mondal <arkamondalofficial@gmail.com>
2024-08-12 00:19:51 +0530

    Added LICENSE declaration

--- src/main.c
+++ src/main.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024, Arka Mondal. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that
+ * can be found in the LICENSE file.
+ */
+
 #include <errno.h>
 #include <getopt.h>
 #include <limits.h>
